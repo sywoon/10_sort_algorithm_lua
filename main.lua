1ec6a247fb849100345e2f74a4e08dfd69dbe864
sywoon <sywoon@163.com>
2018-10-29 10:26:04 +0800

    添加选择排序

--- main.lua
+++ main.lua
@@ -8,25 +8,55 @@ function bubbleSort(arr)
 	log(" 3.重复上述操作，每一轮少最后一个元素，直到剩下一对，排序完")
 
 	log(arr)
+	local times = 0
 	local t = arr
 	for k = 1, #t do
 		for i = 1, #t-k do
+			times = times + 1
 			if t[i] > t[i+1] then
 				t[i], t[i+1] = t[i+1], t[i]
 			end
 		end 
 	end
 	log(arr)
+	log("冒泡排序 总数:" .. #arr .. " 遍历次数:" .. times)
 end
 
+function selectSort(arr)
+	log("----选择排序----")
+	log(" 1.初始无序区为R[1..n],有序区为空；经过n-1躺选择排序得到有序队列")
+	log(" 2.从无序队列中寻找最小数，放入有序队列末尾")
+	log(" 3.每次交换有序和无序索引指向的数据内容，无需额外分配内存")
 
-function run()
+	log(arr)
+	local times = 0
+	local orderIdx = 1
+	for k = 1, #arr-1 do
+		local minIdx = k
+		for i = k+1, #arr do
+			times = times + 1
+			if arr[i] < arr[minIdx] then
+				minIdx = i
+			end
+		end
+		arr[orderIdx], arr[minIdx] = arr[minIdx], arr[orderIdx]
+		orderIdx = orderIdx + 1
+	end
+	log(arr)
+	log("选择排序 总数:" .. #arr .. " 遍历次数:" .. times)
+end
+
+function getTestData()
 	local arr = {}
 	for i = 1, 10 do
 		table.insert(arr, math.random(1, 50))
 	end
+	return arr
+end
 
-	bubbleSort(arr)
+function run()
+	bubbleSort(getTestData())
+	selectSort(getTestData())
 end
 
 run()
