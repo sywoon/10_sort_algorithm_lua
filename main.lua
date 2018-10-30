d4350d7cbe718caeb72366b4333e1bffdac959a0
sywoon <sywoon@163.com>
2018-10-30 10:03:47 +0800

    插入排序

--- main.lua
+++ main.lua
@@ -19,7 +19,7 @@ function bubbleSort(arr)
 		end 
 	end
 	log(arr)
-	log("冒泡排序 总数:" .. #arr .. " 遍历次数:" .. times)
+	log("冒泡排序 总数:" .. #arr .. " 遍历次数:" .. times .. "\n")
 end
 
 function selectSort(arr)
@@ -43,7 +43,37 @@ function selectSort(arr)
 		orderIdx = orderIdx + 1
 	end
 	log(arr)
-	log("选择排序 总数:" .. #arr .. " 遍历次数:" .. times)
+	log("选择排序 总数:" .. #arr .. " 遍历次数:" .. times .. "\n")
+end
+
+function insertSort(arr)
+	log("----插入排序----")
+	log("构建有序序列，对未排序数据在已排序序列中后向前扫描，")
+	log("找到相应位置并插入。")
+	log(" 1.第一个元素可认为在已排序序列")
+	log(" 2.取下一个元素，在已排序序列中从后向前扫描")
+	log(" 3.找到小于等于它的元素，插入其后")
+
+	log(arr)
+	local times = 0
+	for k = 2, #arr do
+		for i = k - 1, 1, -1 do  --已排序序列
+			if arr[i] <= arr[k] or i == 1 then
+				local insIdx = i == 1 and 0 or i
+				--插入位后的元素后移(注意可能插入第一位，即0位后)
+				--新元素放入留出的空位中
+				local v = arr[k]
+				for t = k, insIdx+2, -1 do
+					times = times + 1
+					arr[t] = arr[t-1]
+				end
+				arr[insIdx+1] = v
+				break
+			end
+		end
+	end
+	log(arr)
+	log("插入排序 总数:" .. #arr .. " 遍历次数:" .. times .. "\n")
 end
 
 function getTestData()
@@ -57,6 +87,7 @@ end
 function run()
 	bubbleSort(getTestData())
 	selectSort(getTestData())
+	insertSort(getTestData())
 end
 
 run()
